00ed86449dacc5f0728db14a14911ae6a427dea9
Avijit332 <avijit9thf@gmail.com>
2026-03-09 09:14:31 +0530

    commit

---
+++ datatype.js
@@ -0,0 +1,67 @@
+// //primitive data type
+// let name = "Alice";
+// console.log(typeof name);
+// let age = 30;
+// console.log(typeof age);
+// let isstudent = true;
+// console.log(typeof isstudent);
+// let score;
+// console.log(typeof score);
+// let car = null;
+// console.log(typeof car);
+
+// //non-primitive data type
+// let student ={
+//     name:"Harshita",
+//     age:18
+// }
+// console.log(student);
+
+/*
+//arithmatic operator
+let a=10;
+let b=2;
+console.log(a+b);       //12
+console.log(a-b);       //8
+console.log(a*b);       //20
+console.log(a/b);       //5
+console.log(a%b);       //0
+console.log(++a);       //11
+console.log(a++);       //11
+console.log(--a);       //11
+console.log(a--);       //11
+*/
+/*
+//Assignment operator
+let a=10;
+console.log(a+=1);
+console.log(a-=2);
+console.log(a/=2);
+*/
+/*
+//comparison operator
+let a = 10;
+let b=5;
+console.log(a>b);       //true
+console.log(a!=b);      //true
+*/
+let day="Wednesday";
+switch(day){
+    case "Monday":       
+    console.log("Today is Monday");
+        break;
+    case "Tuesday":       
+    console.log("Today is Tuesday");
+        break;
+    case "Wednesday":       
+    console.log("Today is Wednesday");
+        break;
+    case "Thursday":       
+    console.log("Today is Thursday");
+        break;
+    case "Friday":       
+    console.log("Today is Friday");
+        break;  
+    default:       
+    console.log("Invalid day");
+}
